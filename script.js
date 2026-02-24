ead4ff381cc868e113f9c8654ca1aa1651b6235a
jan24492594 <jan24492594@gmail.com>
2026-02-25 01:14:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function openLetter() {
-  document.querySelector(".page-1").classList.remove("active");
-  document.querySelector(".page-2").classList.add("active");
+  document.querySelector('.page-1').classList.remove('active');
+  document.querySelector('.page-2').classList.add('active');
 }
 
 function flipTicket() {
-  document.getElementById("ticketInner").classList.add("flipped");
+  document.getElementById('ticketInner').classList.add('flipped');
 
   setTimeout(() => {
-    document.querySelector(".page-3").classList.add("active");
-  }, 600);
+    document.querySelector('.page-2').classList.remove('active');
+    document.querySelector('.page-3').classList.add('active');
+
+    setTimeout(() => {
+      document.getElementById('cutesyy').classList.add('show');
+    }, 1500);
+
+  }, 900);
 }
